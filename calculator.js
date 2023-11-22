cba9af3fa98cb4c05db28c28f10b41d9fe6803a7
gebru-mit <gebru2008@gmail.com>
2023-11-22 13:40:07 +0300

    it just calculates

---
+++ calculator.js
@@ -0,0 +1,17 @@
+let x=8
+let y=2
+function calculator(){
+    let sum = x+y
+    let diff = x-y
+    let prod = x*y
+    let div =x/y 
+    console.log("calculator began ...");
+    console.log("sum of: "+ x + " + "+ y +" = "+sum);
+    console.log("difference of: "+ x + " - "+ y +" = "+diff);
+    console.log("product of: "+ x + " * "+ y +" = "+prod); 
+    console.log("division of: "+ x + " / "+ y +" = "+div);
+    console.log("calculator ended ...");
+    console.log("Great!!!");
+
+}
+calculator()
